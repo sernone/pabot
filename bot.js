ee214721d1283bcf893d3fa0f3b858630fac65c6
sernone <sern18@gmail.com>
2018-08-02 14:02:24 -0400

    Added new streamer checkin features

--- bot.js
+++ bot.js
@@ -1,10 +1,26 @@
 var Discord = require("discord.js");
 var auth = require("./auth.json");
 var https = require("https");
+var fs = require("fs");
+var schedule = require("node-schedule");
 var client = new Discord.Client();
 
-var botCmds = [["!role", "Adds a fan role to a user eg: !role pubg"]];
+var botCmds = [
+  ["!role roleName", "Adds a fan role to a user eg: !role pubg"],
+  [
+    "!checkin timeToStream",
+    "For PATV Members only to checkin to their stream for the day"
+  ],
+  [
+    "!streamtoday",
+    "Outputs who is streaming and what time they are streaming on the PATV Channel"
+  ]
+];
 var sentLive = false;
+
+schedule.scheduleJob("0 0 * * *", () => {
+  fs.writeFileSync("./streamer-schedule.json", "{}");
+});
 
 client.on("ready", () => {
   console.log(client.user.username + " - (" + client.user.id + ") Connected");
@@ -16,66 +32,99 @@ client.on("ready", () => {
     channel.send("Command: " + cmd[0] + " - " + cmd[1]);
   });
 
-  setInterval(checkStream,60000,'polaracetv');
+  setInterval(checkStream, 60000, "polaracetv");
 });
 
 client.on("message", message => {
-  if (message.channel.name == "role-call") {
-    var msgChat = message.content;
-    if (msgChat.substring(0, 1) == "!") {
-      var cmd = msgChat.substring(1, msgChat.indexOf(" "));
-      var arg = msgChat.substring(msgChat.indexOf(" ") + 1);
+  var msgChat = message.content;
+  if (msgChat.substring(0, 1) == "!") {
+    var cmd =
+      msgChat.substring(1, msgChat.indexOf(" ")) !== "!"
+        ? msgChat.substring(1, msgChat.indexOf(" "))
+        : msgChat.substring(1);
+    var arg = msgChat.substring(msgChat.indexOf(" ") + 1);
 
-      switch (cmd) {
-        case "role":
-          if (!arg.toLowerCase().endsWith(" fan")) arg = arg + " fan";
-          var fanRoles = message.guild.roles.array();
-          fanRoles = fanRoles.filter(fanRole => fanRole.name.endsWith(" Fan"));
-
-          var roleToAdd = message.guild.roles.filter(
-            fanRole => fanRole.name.toLowerCase() == arg.toLowerCase()
-          );
-
-          if (roleToAdd.size == 1 && roleToAdd.first() != undefined) {
-            var memeber = message.member;
-            var addingRole = roleToAdd.first();
-            if (!memeber.roles.has(addingRole)) {
-              memeber
-                .addRole(addingRole)
-                .then(
-                  message.channel.send(
-                    "Success - Role " +
-                      addingRole +
-                      " has been added given to you"
-                  )
-                )
-                .catch(err => {
-                  message.channel.send(
-                    "An error occured please contact an admin to have them check the error. Apologies we'll get this fixed soon!"
-                  );
-                  console.error(err);
-                });
-            }
-          } else {
-            message.channel.send(
-              "Error: Role " +
-                arg.toLowerCase() +
-                " not found. Check available roles and try again or contact an admin. Available Roles are: " +
-                fanRoles.join()
+    switch (cmd.toLowerCase()) {
+      case "checkin":
+        if (message.channel.name == "patv-streamers") {
+          if (arg.substring(1) !== cmd) {
+            var streamer = message.author.username;
+            var sch = JSON.parse(
+              fs.readFileSync("./streamer-schedule.json", "utf8")
             );
-          }
-          break;
-        case '!':
-          message.channel.send(
-            "All commands require argements, the command you used is missing an argument"
-          );
+            sch[streamer] = arg;
+            fs.writeFileSync("./streamer-schedule.json", JSON.stringify(sch));
+            message.reply("You have checked to stream today for " + arg);
+          } else
+            message.channel.send(
+              "Hey streamer!, the !checkin command requires you put when your steaming please!:"
+            );
+        }
         break;
-        default:
-          message.channel.send(
-            "Invalid command, available commands are: !role"
-          );
-          break;
-      }
+      case "streamtoday":
+        var sch = "";
+        var data = JSON.parse(
+          fs.readFileSync("./streamer-schedule.json", "utf8")
+        );
+        for (streamer in data) {
+          sch +=
+            streamer + " is set to stream " + data[streamer] + " today. \n";
+        }
+
+        message.channel.send(sch);
+        break;
+      case "role":
+        if (message.channel.name == "role-call") {
+          if (arg.substring(1) !== cmd) {
+            if (!arg.toLowerCase().endsWith(" fan")) arg = arg + " fan";
+            var fanRoles = message.guild.roles.array();
+            fanRoles = fanRoles.filter(fanRole =>
+              fanRole.name.endsWith(" Fan")
+            );
+
+            var roleToAdd = message.guild.roles.filter(
+              fanRole => fanRole.name.toLowerCase() == arg.toLowerCase()
+            );
+
+            if (roleToAdd.size == 1 && roleToAdd.first() != undefined) {
+              var memeber = message.member;
+              var addingRole = roleToAdd.first();
+              if (!memeber.roles.has(addingRole)) {
+                memeber
+                  .addRole(addingRole)
+                  .then(
+                    message.channel.send(
+                      "Success - Role " +
+                        addingRole +
+                        " has been added given to you"
+                    )
+                  )
+                  .catch(err => {
+                    message.channel.send(
+                      "An error occured please contact an admin to have them check the error. Apologies we'll get this fixed soon!"
+                    );
+                    console.error(err);
+                  });
+              }
+            } else {
+              message.channel.send(
+                "Error: Role " +
+                  arg.toLowerCase() +
+                  " not found. Check available roles and try again or contact an admin. Available Roles are: " +
+                  fanRoles.join()
+              );
+            }
+          } else
+            message.channel.send(
+              "The !role command requires a fan role after it. Please include the role you're looking for after the command."
+            );
+        }
+        break;
+      default:
+        message.channel.send(
+          "Invalid command, available commands are: !role, !checkin, !streamtoday"
+        );
+        break;
     }
   }
 });
@@ -85,28 +134,35 @@ client.login(auth.token);
 //Other functions
 function checkStream(twitchUser) {
   var http_options = {
-    "host": "api.twitch.tv",
-    "path": "/helix/streams?user_login="+twitchUser,
-    "method": "GET",
-    "headers": {
+    host: "api.twitch.tv",
+    path: "/helix/streams?user_login=" + twitchUser,
+    method: "GET",
+    headers: {
       "Client-ID": "btwox7o0tmnpqupua8xwj8rxrt94x7"
     }
-  }
-  https.get(http_options, (resp) =>{
-    var data = '';
+  };
+  https.get(http_options, resp => {
+    var data = "";
 
-    resp.on('data', (chunk) => {
+    resp.on("data", chunk => {
       data += chunk;
     });
 
-    resp.on('end', () =>{
+    resp.on("end", () => {
       var chanData = JSON.parse(data);
       chanData = chanData.data;
 
-      if(chanData.length > 0) {
-        if(!sentLive) {
-          var disChan = client.channels.find("name", "stream-and-media-sharing");
-          disChan.send('We are LIVE here https://twitch.tv/'+twitchUser+' , Come by and check us out!');
+      if (chanData.length > 0) {
+        if (!sentLive) {
+          var disChan = client.channels.find(
+            "name",
+            "stream-and-media-sharing"
+          );
+          disChan.send(
+            "We are LIVE here https://twitch.tv/" +
+              twitchUser +
+              " , Come by and check us out!"
+          );
           disChan.send(chanData[0].title);
           sentLive = true;
         }
